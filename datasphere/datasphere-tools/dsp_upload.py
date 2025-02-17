b2d709ba50e54a3a071da75840107b2032b9341a
ML <pumping@gmx.de>
2025-02-17 16:03:50 +0100

    Update

--- datasphere/datasphere-tools/dsp_upload.py
+++ datasphere/datasphere-tools/dsp_upload.py
@@ -13,26 +13,18 @@ except Exception as e:
     print(f"An unexpected error occurred: {e}")
 
 dsp_host  = json_data["dsp_host"]
-print(f'Host: {dsp_host}')
-
-dsp_space  = json_data["dsp_space"]
-print(f'Space: {dsp_space}')
 
 # Default upload file
 dsp_upload_file = r"datasphere\datasphere-tools\Local Tests\Target Data.json"
 
-# command = f'datasphere objects local_tables create --technical-name TEST_CLI --space {dsp_space} --file-path {dsp_upload_file} --verbose'
+# Run datasphere-cli command (static)
+# command = f'datasphere objects local_tables create --technical-name TEST_CLI --space PLAYGROUND --file-path {dsp_upload_file} --verbose'
 # subprocess.run(command, shell=True)
 
-# subprocess.run(["powershell", "/c", "node -v"], shell=False)
-# C:\\Users\\demlotter\\OneDrive - Brenntag\\Datasphere\\CLI Development\node-v22.13.1-win-x64\
-
-
-# CRUD JSON commands:
-# datasphere objects local-tables create --space INGESTION_001 --technical-name TEST_CLI --file-path "C:\Users\demlotter\OneDrive - Brenntag\Datasphere\CLI Development\Python Workspace\datasphere\datasphere-tools\Templates\local_table_delta_test.json"
-# datasphere objects local-tables update --space INGESTION_001 --technical-name TEST_CLI --file-path "C:\Users\demlotter\OneDrive - Brenntag\Datasphere\CLI Development\Python Files\Target JSON.json"
-# datasphere objects local-tables read   --space INGESTION_001 --technical-name TEST_CLI --output "C:\Users\demlotter\OneDrive - Brenntag\Datasphere\CLI Development\Python Workspace\datasphere\datasphere-tools\Templates\local_table.json"
-
-
-# datasphere objects local-tables read --space PLAYGROUND --technical-name Template_Table --output "C:\Users\demlotter\OneDrive - Brenntag\Datasphere\CLI Development\Python Workspace\datasphere\datasphere-tools\Templates\Template_Table.json"
-# datasphere objects local-tables create --space PLAYGROUND --technical-name Template_Table2 --file-path "C:\Users\demlotter\OneDrive - Brenntag\Datasphere\CLI Development\Python Workspace\datasphere\datasphere-tools\Templates\local_table_delta_test.json"
+# Run datasphere-cli command (dynamic)
+# dsp_space = input("Please provide the space name : ")
+# dsp_update_type = input("Please provide the update type [create] [update] : ")
+# dsp_technical_name = input("Please provide the technical-name : ")
+# print(f'datasphere objects local_tables {dsp_update_type} --technical-name {dsp_technical_name} --space {dsp_space} --file-path {dsp_upload_file} --verbose')
+# command = f'datasphere objects local_tables {dsp_update_type} --technical-name {dsp_technical_name} --space {dsp_space} --file-path {dsp_upload_file} --verbose'
+# subprocess.run(command, shell=True)
