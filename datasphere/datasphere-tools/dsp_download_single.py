86bfc73a452a0b8bfec71e62d7764750d3d4b51d
ML <pumping@gmx.de>
2025-02-18 11:41:11 +0100

    Update

--- datasphere/datasphere-tools/dsp_download_single.py
+++ datasphere/datasphere-tools/dsp_download_single.py
@@ -3,5 +3,5 @@ import subprocess
 dsp_technical_name = input("Please provide a technical name : ")
 dsp_output_path = r"datasphere\datasphere-tools\DSP_Json_Downloads"
 
-command = f'datasphere objects local-tables read --technical-name {dsp_technical_name } --space INGESTION_001 --output "{dsp_output_path}\{dsp_technical_name }.json"'
+command = f'datasphere objects local-tables read --technical-name {dsp_technical_name} --space INGESTION_001 --output "{dsp_output_path}\{dsp_technical_name}.json"'
 subprocess.run(command, shell=True)
