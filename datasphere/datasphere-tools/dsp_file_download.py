46d16ef756e8e7087d35eefab838efc1fae338ea
ML <pumping@gmx.de>
2025-02-20 12:22:44 +0100

    Update

--- datasphere/datasphere-tools/dsp_file_download.py
+++ datasphere/datasphere-tools/dsp_file_download.py
@@ -2,6 +2,8 @@ import subprocess
 
 dsp_output_path = r"datasphere\datasphere-tools\dsp_files\dsp_downloads"
 dsp_technical_name = input("Please provide a technical name : ")
+dsp_object_type = input("Please provide an object type[default: local-tables] : ").strip() or 'local-tables'
+dsp_space = input("Please provide a space name[default: INGESTION_001] : ").strip() or 'INGESTION_001'
 dsp_accept = input("Please provide accept parameter[optional] : ") 
     # E.g.: 
     # "application/vnd.sap.datasphere.object.content+json"             (default)
@@ -9,10 +11,10 @@ dsp_accept = input("Please provide accept parameter[optional] : ")
     # "application/vnd.sap.datasphere.object.content.run-time+json"    (like default)
 
 if dsp_accept:
-    command = f'datasphere objects local-tables read --technical-name {dsp_technical_name} --space PLAYGROUND --accept {dsp_accept} --output "{dsp_output_path}\\{dsp_technical_name}.json" --verbose'
-    print(command)
+    command = f'datasphere objects local-tables read --technical-name {dsp_technical_name} --space {dsp_space} --accept {dsp_accept} --output "{dsp_output_path}\\{dsp_technical_name}.json" --verbose'
+    print(f'Datasphere command: {command}')
     subprocess.run(command, shell=True)
 else: 
-    command = f'datasphere objects local-tables read --technical-name {dsp_technical_name} --space PLAYGROUND --output "{dsp_output_path}\\{dsp_technical_name}.json" --verbose'
-    print(command)
+    command = f'datasphere objects local-tables read --technical-name {dsp_technical_name} --space {dsp_space} --output "{dsp_output_path}\\{dsp_technical_name}.json" --verbose'
+    print(f'Datasphere command: {command}')
     subprocess.run(command, shell=True)
